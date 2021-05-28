abf368243d8b09da50d58efaba2ce7fa43180524
PipesSex <pipesjimenez6@gmail.com>
2021-05-28 16:03:16 -0500

    Agregando el segundo panel

--- src/main/java/Vista/PanelPersona.java
+++ src/main/java/Vista/PanelPersona.java
@@ -10,10 +10,10 @@ package Vista;
  *
  * @author pipesex
  */
-public class vista extends javax.swing.JFrame {
+public class PanelPersona extends javax.swing.JFrame {
 
     /** Creates new form vista */
-    public vista() {
+    public PanelPersona() {
         initComponents();
     }
 
@@ -462,20 +462,21 @@ public class vista extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(vista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PanelPersona.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(vista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PanelPersona.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(vista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PanelPersona.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(vista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PanelPersona.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new vista().setVisible(true);
+                new PanelPersona().setVisible(true);
             }
         });
     }
